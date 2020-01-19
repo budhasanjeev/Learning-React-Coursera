9a582aa4b0b4f8b55e2f2c1e9c217266438ec53f
Sanjeev Budha <budhasanjeev@gmail.com>
2020-01-19 15:41:10 +0900

    SPA part 2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
+import { Card, CardImg, CardBody, CardTitle, CardText, Breadcrumb, BreadcrumbItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 
   function RenderDish({dish}) 
@@ -25,7 +26,7 @@ import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
         <ul className="list-unstyled">
 
         {comments.map((comment) => {
-          return (<li>
+          return (<li key={comment.id}>
             <p> {comment.comment }</p>
             <p> --{comment.author}, {new Intl.DateTimeFormat('en-US', { year:'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))} </p>
           </li>)
@@ -42,8 +43,18 @@ import { Card, CardImg, CardBody, CardTitle, CardText } from 'reactstrap';
       return(
         <div className="container">
           <div className="row">
+            <Breadcrumb>
+              <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+              <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+            </Breadcrumb>
+            <div className="col-12">
+              <h3>{props.dish.name}</h3>
+              <hr />
+            </div>
+          </div>
+          <div className="row">
             <RenderDish dish={props.dish} />
-            <RenderComments comments={props.dish.comments} />
+            <RenderComments comments={props.comments} />
           </div>
         </div>
       );
